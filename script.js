ddfd3a4b74d34adc2671ff3802bd7a7e4da1859c
Mel Huang <hello@melaniehuang.com>
2025-12-08 22:47:29 +1100

    adding aoc and cookies

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.onload = () => {
+    let aoc = getCookie("aoc");
+    if (aoc != "") {
+        document.getElementById("loading").style.display = 'none';
+    } else {
+        document.cookie = "aoc=seen";
+    }
+
     let places = staticLoadPlaces();
     renderPlaces(places);
 
@@ -11,6 +18,22 @@ window.onload = () => {
         }
     });
 };
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 function staticLoadPlaces() {
     return [
